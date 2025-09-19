31beae7b36b483a1298ef748236802d3340db5ee
victor <vickydollas11@gmail.com>
2025-09-19 11:10:15 +0100

    Edit ftn, clear all ftn

--- index.js
+++ index.js
@@ -3,10 +3,9 @@ const todoList = [{
     listDate: ''
 }]
 const inputText = document.querySelector('.inputText')
-const inputDate = document.querySelector('.inputDate')
+// const inputDate = document.querySelector('.inputDate')
 const inputButton = document.querySelector('.trigger')
 const displayList = document.querySelector('.display-to-do-1')
-
 let draggedItem = null
 
 displayHTML()
@@ -15,14 +14,20 @@ function displayHTML(){
     for (let i = 0; i < todoList.length; i++){
         const name = todoList[i]
         const listName = name.listName
-        const listDate = name.listDate
+        // const listDate = name.listDate
         const html = `
             <div class="list-item" draggable="true" data-index="${i}">
-                <p>${listName}<span class="separation">${listDate}</span></p>
-                <button onclick="
+                <p>${listName}</p>
+                <div>
+                    <img src="./images/delete.svg" onclick="
                     todoList.splice(${i}, 1);
                     displayHTML();
-                " class="js-button">delete</button>
+                " class="js-button">
+                    <img src="./images/edit.svg" onclick="
+                    editItem(${i})
+                "
+                    class="js-button-1">
+                </div>
             </div>
             `
         pushHTML = pushHTML + html
@@ -30,12 +35,37 @@ function displayHTML(){
     displayList.innerHTML = pushHTML
     addDragEvents()
 }
+function editItem(index) {
+    const item = document.querySelector(`[data-index="${index}"]`);
+    const originalText = todoList[index].listName;
+    
+    // Replace the paragraph with an input field
+    item.innerHTML = `
+        <input type="text" class="edit-input" value="${originalText}">
+        <div>
+            <img src="./images/save.svg" onclick="saveEdit(${index})" class="js-button-1">
+            <img src="./images/delete.svg" onclick="todoList.splice(${index}, 1); displayHTML();" class="js-button">
+        </div>
+    `;
+    
+    // Set focus on the input field for immediate editing
+    item.querySelector('.edit-input').focus();
+}
+
+function saveEdit(index) {
+    const editedInput = document.querySelector(`[data-index="${index}"] .edit-input`);
+    const newValue = editedInput.value;
+
+    if (newValue) {
+        todoList[index].listName = newValue;
+        displayHTML(); // Re-render the list with the new value
+    }
+}
+
 function getInputButton() {
     const inputtedValue = inputText.value
-    const inputtedDate = inputDate.value
     todoList.push({
-        listName: inputtedValue , 
-        listDate: inputtedDate
+        listName: inputtedValue
     })
     inputText.value = ''
     displayHTML()
@@ -86,23 +116,3 @@ function getDragAfterElement(container, y) {
 }
 
 inputButton.addEventListener('click', getInputButton);
-
-inputButton.forEach(
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-)
